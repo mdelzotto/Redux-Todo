3e34badc7e38d5bd9a1000c0eb4d2910e34d0665
mdelzotto <madelzotto@comcast.net>
2019-05-15 10:38:26 -0400

    completed redux-todo without css styling

---
+++ todo/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
+import { addTodo, toggleTodo, deleteTodo } from './actions'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My Todo List!</h1>
+        <Todos
+          todos={this.props.todos}
+          toggleTodo={this.props.toggleTodo}
+          deleteTodo={this.props.deleteTodo}
+        />
+        <TodoForm
+          addTodo={this.props.addTodo}
+          todos={this.props.todos}
+        />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  todos: state.todos.todos
+})
+
+const mapActionsToProps = {
+  addTodo,
+  toggleTodo,
+  deleteTodo
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(App)
---
+++ todo/src/components/Todos.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Todos.css'
+
+const Todos = (props) => {
+  const items = props.todos.map(item => item)
+  return (
+    <div>
+      <h2>Todos Component</h2>
+      <ul>
+        {
+          items.map((todo, id)  =>{
+            return (
+              <li
+                className={`todo-item ${todo.completed ? 'lineThrough' : ''}`}
+                key={`todo-${todo.id}`}
+                onClick={() => props.toggleTodo(id)}
+                 //onClick={() => props.toggleTodo(todo.id)}, USING THE TODO'S ID
+                 //style={{textDecoration: todo.completed ? 'line-through' : 'none'}}, USING INLINE STYLES
+              >
+                {todo.value}
+                <button
+                  onClick={() => props.deleteTodo(todo.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Todos
